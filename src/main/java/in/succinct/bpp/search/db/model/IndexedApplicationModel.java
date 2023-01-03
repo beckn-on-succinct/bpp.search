3c47a5392d7e362aa08a72fe5bb3b5a9d0f517c5
Venky <venkatramanm@gmail.com>
2023-01-03 23:23:37 +0530

    Size increase

--- src/main/java/in/succinct/bpp/search/db/model/IndexedApplicationModel.java
+++ src/main/java/in/succinct/bpp/search/db/model/IndexedApplicationModel.java
@@ -27,7 +27,7 @@ public interface IndexedApplicationModel {
     public void setObjectName(String objectName);
 
 
-    @COLUMN_SIZE(1024*2)
+    @COLUMN_SIZE(1024*4)
     public String getObjectJson();
     public void setObjectJson(String objectJson);
 }
