f7793d24f27f141b8c515a0a368228fc3557fde6
Venky <venkatramanm@gmail.com>
2025-03-11 00:30:33 +0530

    Use BG For caching catalog

--- src/main/java/in/succinct/bpp/search/extensions/SearchExtensionInstaller.java
+++ src/main/java/in/succinct/bpp/search/extensions/SearchExtensionInstaller.java
@@ -1,144 +1,59 @@
 package in.succinct.bpp.search.extensions;
 
+import com.venky.core.string.StringUtil;
 import com.venky.core.util.ObjectUtil;
 import com.venky.extension.Extension;
 import com.venky.extension.Registry;
 import com.venky.swf.db.Database;
 import com.venky.swf.db.annotations.column.ui.mimes.MimeType;
-import com.venky.swf.db.model.CryptoKey;
 import com.venky.swf.db.model.application.Application;
-import com.venky.swf.db.model.application.ApplicationPublicKey;
-import com.venky.swf.db.model.application.Event;
-import com.venky.swf.db.model.application.api.EndPoint;
-import com.venky.swf.db.model.application.api.EventHandler;
-import com.venky.swf.db.model.reflection.ModelReflector;
+import com.venky.swf.integration.api.Call;
+import com.venky.swf.integration.api.HttpMethod;
+import com.venky.swf.integration.api.InputFormat;
 import com.venky.swf.path._IPath;
 import com.venky.swf.plugins.background.core.DbTask;
 import com.venky.swf.plugins.background.core.TaskManager;
-import com.venky.swf.plugins.beckn.messaging.Subscriber;
-import com.venky.swf.plugins.collab.db.model.participants.admin.Company;
 import com.venky.swf.routing.Config;
-import com.venky.swf.sql.Expression;
-import com.venky.swf.sql.Operator;
-import com.venky.swf.sql.Select;
 import in.succinct.beckn.Catalog;
 import in.succinct.beckn.Context;
 import in.succinct.beckn.Message;
 import in.succinct.beckn.Providers;
 import in.succinct.beckn.Request;
+import in.succinct.beckn.Subscriber;
 import in.succinct.bpp.core.adaptor.CommerceAdaptor;
 import in.succinct.bpp.core.adaptor.api.NetworkApiAdaptor;
-import in.succinct.catalog.indexer.db.model.Item;
-import in.succinct.catalog.indexer.db.model.Provider;
-import in.succinct.catalog.indexer.ingest.CatalogDigester.Operation;
 import in.succinct.onet.core.adaptor.NetworkAdaptor;
-import org.jetbrains.annotations.NotNull;
+import in.succinct.onet.core.adaptor.NetworkAdaptor.Domain;
+import org.json.simple.JSONAware;
 
-import java.sql.Timestamp;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.UUID;
 import java.util.logging.Level;
-import java.util.stream.Collectors;
 
 public class SearchExtensionInstaller implements Extension {
     static {
         Registry.instance().registerExtension("in.succinct.bpp.search.extension.installer",new SearchExtensionInstaller());
+        Registry.instance().registerExtension("in.succinct.bpp.search.extension.reinstall",new SearchExtensionInstaller());
     }
     @Override
     public void invoke(Object... context) {
         NetworkAdaptor networkAdaptor = (context.length > 0) ? (NetworkAdaptor) context[0] : null;
         CommerceAdaptor adaptor = (context.length > 1) ? (CommerceAdaptor) context[1] :null;
         Application application = (context.length > 2) ? (Application)context[2] : null;
-        installPlugin(networkAdaptor,adaptor,application);
+        if (adaptor != null) {
+            indexItems(networkAdaptor, adaptor);
+        }
     }
-    public void installPlugin(NetworkAdaptor networkAdaptor, @NotNull CommerceAdaptor adaptor, Application app){
-        Subscriber subscriber = adaptor.getSubscriber();
 
-        // Create App for self
-        Application application  = app;
-        if (application == null) {
-            Company company = adaptor.createCompany(adaptor.getProviderConfig().getOrganization(),adaptor.getSubscriber().getSubscriberId());
-            application = adaptor.createApplication(company,adaptor.getSubscriber().getSubscriberId(),null);
-        }
-
-
-        //CryptoKey cryptoKey = CryptoKey.find(subscriber.getCryptoKeyId(), CryptoKey.PURPOSE_SIGNING);
-
-        ApplicationPublicKey publicKey = Database.getTable(ApplicationPublicKey.class).newRecord();
-        publicKey.setApplicationId(application.getId());
-        publicKey.setPurpose(CryptoKey.PURPOSE_SIGNING);
-        publicKey.setAlgorithm(Request.SIGNATURE_ALGO);
-        publicKey.setKeyId(subscriber.getUniqueKeyId());
-        publicKey.setValidFrom(new Timestamp(subscriber.getValidFrom().getTime()));
-        publicKey.setValidUntil(new Timestamp(subscriber.getValidTo().getTime()));
-        publicKey.setPublicKey(Request.getPemSigningKey(subscriber.getSigningPublicKey()));
-        publicKey = Database.getTable(ApplicationPublicKey.class).getRefreshed(publicKey);
-        publicKey.save();
-
-        EndPoint endPoint = Database.getTable(EndPoint.class).newRecord();
-        endPoint.setApplicationId(application.getId());
-        endPoint.setBaseUrl(subscriber.getSubscriberUrl());
-        endPoint = Database.getTable(EndPoint.class).getRefreshed(endPoint);
-        endPoint.save();
-
-        for (String eventName : new String[]{CATALOG_SYNC_EVENT,CATALOG_SYNC_ACTIVATE, CATALOG_SYNC_DEACTIVATE}){
-            Event event = Database.getTable(Event.class).newRecord();
-            event.setName(eventName);
-            event = Database.getTable(Event.class).getRefreshed(event);
-            event.save();
-
-            EventHandler eventHandler = Database.getTable(EventHandler.class).newRecord();
-            eventHandler.setApplicationId(application.getId());
-            eventHandler.setEndPointId(endPoint.getId());
-            eventHandler.setEventId(event.getId());
-            eventHandler.setContentType(MimeType.APPLICATION_JSON.toString());
-            eventHandler.setRelativeUrl(String.format("hook/providers/%s",eventName.substring("catalog_".length()))); //Dummy action needed to next from subscriber url. Url is needed to detect registry usually
-            eventHandler = Database.getTable(EventHandler.class).getRefreshed(eventHandler);
-            eventHandler.save();
-
-        }
-
-        indexItems(networkAdaptor,adaptor,subscriber.getSubscriberId());
-    }
-    public static final String CATALOG_SYNC_EVENT = "catalog_ingest";
-    public static final String CATALOG_SYNC_ACTIVATE = "catalog_" + Operation.activate.name();
-    public static final String CATALOG_SYNC_DEACTIVATE = "catalog_" + Operation.deactivate.name();
-
-    private void indexItems(NetworkAdaptor networkAdaptor,CommerceAdaptor adaptor, String subscriberId) {
-        Select select  = new Select().from(Provider.class);
-        select.where(new Expression(select.getPool(),"SUBSCRIBER_ID",Operator.EQ,subscriberId));
-        List<Provider> providerList = select.execute();
-        if (!providerList.isEmpty()){
-            new Select().from(Item.class).where(new Expression(ModelReflector.instance(Item.class).getPool(),"ACTIVE", Operator.EQ)).execute(Item.class).forEach(i->{
-                i.setActive(true);i.save();
-            });
-            return;
-        }
-        Event event = Event.find(CATALOG_SYNC_EVENT);
-        if (event == null){
-            Config.instance().getLogger(getClass().getName()).log(Level.WARNING, CATALOG_SYNC_EVENT + ": event not defined");
-            return;
-        }
-        Set<Long> endPoints = adaptor.getApplication().getEndPoints().stream().filter(ep -> {
-            return ObjectUtil.equals(ep.getBaseUrl(), adaptor.getSubscriber().getSubscriberUrl()) ||
-                    ObjectUtil.equals(ep.getBaseUrl(), networkAdaptor.getBaseUrl()) ||
-                    ObjectUtil.equals(ep.getBaseUrl(), networkAdaptor.getSearchProvider().getSubscriberUrl());
-        }).map(ep -> ep.getId()).collect(Collectors.toSet());
-
-
-        List<EventHandler> catalogSyncHandlers = adaptor.getApplication().getEventHandlers().stream().filter(eh -> {
-            return endPoints.contains(eh.getEndPointId()) && eh.getEventId() == event.getId();
-        }).collect(Collectors.toList());
-
-        if (catalogSyncHandlers.isEmpty()){
-            Config.instance().getLogger(getClass().getName()).log(Level.WARNING,"Catalog not indexed as there are no handlers installed for " + CATALOG_SYNC_EVENT);
-            return;
-        }
-
+    private void indexItems(NetworkAdaptor networkAdaptor,CommerceAdaptor adaptor) {
+        String subscriberId = adaptor.getSubscriber().getSubscriberId();
         Request response = new Request();
         ((NetworkApiAdaptor)networkAdaptor.getApiAdaptor())._search(adaptor,response);
         Providers providers = response.getMessage().getCatalog().getProviders();
@@ -153,10 +68,46 @@ public class SearchExtensionInstaller implements Extension {
                 context.remove(_IPath.class.getName());
                 Database.getInstance().setContext(context);
             }
-            event.raise(catalogSyncHandlers,prepareCatalogSyncRequest(providers,adaptor,networkAdaptor));
+            Request request = prepareCatalogSyncRequest(providers,adaptor,networkAdaptor);
+            Request networkRequest = networkAdaptor.getObjectCreator(adaptor.getSubscriber().getDomain()).create(Request.class);
+            networkRequest.update(request); // Updates in network format
+            call_bg(adaptor,getGateways(networkAdaptor),networkRequest,new HashMap<>());
+            
         },false);
 
     }
+    
+    static List<Subscriber> getGateways(NetworkAdaptor networkAdaptor){
+        return networkAdaptor.lookup(new in.succinct.beckn.Subscriber() {{
+            //setSubscriberId(networkAdaptor.getSearchProviderId());
+            setType(Subscriber.SUBSCRIBER_TYPE_BG);
+        }}, true);
+    }
+    
+    static <J extends JSONAware> List<J> call_bg(CommerceAdaptor adaptor , List<Subscriber> gateways, Request request, Map<String,String> additionalHeaders){
+        List<J> responses = new ArrayList<>();
+        for (Subscriber gwSubscriber : gateways) {
+            if (!ObjectUtil.equals(gwSubscriber.getStatus(), Subscriber.SUBSCRIBER_STATUS_SUBSCRIBED)) {
+                continue;
+            }
+            Call<InputStream> call = new Call<InputStream>().url(gwSubscriber.getSubscriberUrl(), request.getContext().getAction()).headers(additionalHeaders).
+                    headers(new HashMap<>(){{
+                        put("Content-Type", MimeType.APPLICATION_JSON.toString());
+                        if (request.getContext().getAction().equals("on_search")) {
+                            put("Authorization", request.generateAuthorizationHeader(adaptor.getSubscriber().getSubscriberId(), adaptor.getSubscriber().getPubKeyId()));
+                        }
+                    }}).inputFormat(InputFormat.INPUT_STREAM).method(HttpMethod.POST).input(new ByteArrayInputStream(request.toString().getBytes(StandardCharsets.UTF_8)));
+            if (!call.hasErrors()) {
+                Config.instance().getLogger(Catalog.class.getName()).log(Level.INFO, String.format("BroadCasted %s to bg %s :\n%s", request.getContext().getAction(), gwSubscriber.getSubscriberId(),
+                        StringUtil.valueOf(call.getResponseStream())));
+                responses.add(call.getResponseAsJson());
+            } else {
+                responses.add(null);
+            }
+        }
+        return responses;
+    }
+    
     public Request prepareCatalogSyncRequest(Providers providers,CommerceAdaptor adaptor,NetworkAdaptor networkAdaptor){
         Request request = new Request();
         Context context = new Context();
@@ -183,5 +134,5 @@ public class SearchExtensionInstaller implements Extension {
         return request;
 
     }
-
+    
 }
